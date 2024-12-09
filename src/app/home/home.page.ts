97e39addd0d90728447278dd014ea5a12fab8df5
GitShit90 <raultomy2003@gmail.com>
2024-12-09 23:32:08 +0100

    login eta main

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  email: string = '';
+  password: string = '';
+  errorMessage: string = ''; // Variable para el mensaje de error
 
-  constructor() {}
+  constructor(private navCtrl: NavController) {}
 
+  login() {
+    if (
+      (this.password === 'admin' && this.email === 'admin@gmail.com') ||
+      (this.password === 'user' && this.email === 'user@gmail.com')
+    ) {
+      this.navCtrl.navigateForward('/menu');
+    } else if (
+      (this.password !== 'user' && this.email === 'user@gmail.com') ||
+      (this.password !== 'admin' && this.email === 'admin@gmail.com')
+    ) {
+      this.errorMessage = 'Contraseña incorrecta.';
+    } else {
+      this.errorMessage = 'Correo o contraseña incorrectos.';
+    }
+  }
 }
