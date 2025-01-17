88cbaaf3aafc2941adae065557a7861fbe18b2b7
rgomezh23 <rgomezh23@fpsanturtzilh.eus>
2025-01-17 11:46:25 +0100

    inventario traducido

--- src/app/inventario/inventario.page.ts
+++ src/app/inventario/inventario.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-inventario',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventarioPage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+ public inventoryTitle: string = '';
+   public productLabel: string = '';
+   public materialLabel: string = '';
+ 
+   constructor(private translateService: TranslateService) {
+     // Establecer el idioma predeterminado
+     this.translateService.setDefaultLang('es');
+     this.translateService.use('es'); // Usar el idioma predeterminado
+   }
+ 
+   ngOnInit() {
+     this.translateLabels();
+   }
+ 
+   // Método para cargar las traducciones
+   translateLabels() {
+     this.translateService.get([
+       'INVENTORY.TITLE',
+       'INVENTORY.PRODUCT',
+       'INVENTORY.MATERIAL',
+     ]).subscribe(translations => {
+       this.inventoryTitle = translations['INVENTORY.TITLE'];
+       this.productLabel = translations['INVENTORY.PRODUCT'];
+       this.materialLabel = translations['INVENTORY.MATERIAL'];
+    
+     });
+   }
+ 
+   // Método para cambiar el idioma dinámicamente
+   changeLanguage(lang: string) {
+     this.translateService.use(lang); // Cambiar el idioma
+     this.translateLabels(); // Recargar las traducciones
+   }
 }
