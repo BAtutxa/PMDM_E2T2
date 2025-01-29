499dc807a7d6883a31e85b4e4af9e7db40ab07bb
agonzalebr <agonzalezb23@fpsanturtzilh.eus>
2025-01-29 13:25:09 +0100

    boton bueltatu y logout arreglado

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -14,27 +14,10 @@ export class FooterComponent implements OnInit {
 
   constructor(private router: Router, private ubikazioa: Location) {}
 
-  // Método para ir a la página Home o Menu
-  homebueltatu() {
-    const url = this.router.url;
-
-    if (url !== '/menu') {
-      this.router.navigate(['/menu']);
-    } else {
-      this.router.navigate(['/home']);
-    }
-  }
-
-  // Método para mostrar el botón de Home cuando no estamos en la página Home ni en Calendario
-  homebotoiaAgertu() {
-    const url = this.router.url;
-    this.agertuHome = url !== '/home' && url !== '/calendario' && url !== '/calendario';
-  }
-
   // Método para mostrar el botón de Volver cuando no estamos en la página Calendario
   bueltatubotoiaAgertu() {
     const url = this.router.url;
-    this.agertuBueltatu = url !== '/home';  // El botón de Volver aparece si no estamos en /calendario
+    this.agertuBueltatu = url == '/productos';
   }
 
   // Método para navegar hacia atrás
@@ -54,12 +37,12 @@ export class FooterComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.homebotoiaAgertu();
+
     this.bueltatubotoiaAgertu();
     this.logoutAgertu();  // Asegúrate de llamar a logoutAgertu al inicializar el componente
 
     this.router.events.subscribe(() => {
-      this.homebotoiaAgertu();
+
       this.bueltatubotoiaAgertu();
       this.logoutAgertu();  // Llamar siempre que cambie la ruta
     });
