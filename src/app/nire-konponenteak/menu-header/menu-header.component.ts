47fa961f58c0e4f74bec6918bad1465eff24e8b4
Nora <nely23@fpsanturtzilh.eus>
2025-01-30 13:05:23 +0100

    menu ARREGLADO!

--- src/app/nire-konponenteak/menu-header/menu-header.component.ts
+++ src/app/nire-konponenteak/menu-header/menu-header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-menu-header',
