417995550a14a70d153a819c938137c693989964
rgomezh23 <rgomezh23@fpsanturtzilh.eus>
2025-02-04 12:54:27 +0100

    añade historiala

---
+++ src/app/historiala/historiala.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-historiala',
+  templateUrl: './historiala.page.html',
+  styleUrls: ['./historiala.page.scss'],
+})
+export class HistorialaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
