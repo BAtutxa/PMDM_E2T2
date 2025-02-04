8f60b22bf0558989552fbb130f78237f9b9ae29c
rgomezh23 <rgomezh23@fpsanturtzilh.eus>
2025-02-04 13:28:36 +0100

    pone bonito el historial para hacer las bases del historial no se.

--- src/app/historiala/historiala.page.ts
+++ src/app/historiala/historiala.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-historiala',
   templateUrl: './historiala.page.html',
-  styleUrls: ['./historiala.page.scss'],
+  styleUrls: ['../administracion/administracion.page.scss'],
 })
 export class HistorialaPage implements OnInit {
 
