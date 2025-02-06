4fbfe6be8a4f68360d973b503ce281c1e4fdc1a3
rgomezh23 <rgomezh23@fpsanturtzilh.eus>
2025-02-06 16:53:10 +0100

    añade modo oscuro🌚🌚🌚

--- src/app/nire-konponenteak/menu-header/menu-header.component.ts
+++ src/app/nire-konponenteak/menu-header/menu-header.component.ts
@@ -1,27 +1,69 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
+import { ModoOscuroService } from 'src/app/services/ModoOscuro.service';
 
 @Component({
   selector: 'app-menu-header',
   templateUrl: './menu-header.component.html',
   styleUrls: ['./menu-header.component.scss'],
 })
-export class MenuHeaderComponent  implements OnInit {
-
+export class MenuHeaderComponent implements OnInit {
   esProfe: Boolean = false;
-  constructor(private userService:UserService) { }
+  modoOscuro: Boolean = false;
+
+  constructor(
+    private userService: UserService,
+    private modoOscuroService: ModoOscuroService
+  ) {}
 
   ngOnInit() {
     this.mirarPrivilegios();
-
+    this.cargarModoPreferido();  // Cargar el modo preferido al iniciar
   }
 
-  mirarPrivilegios(){
+  mirarPrivilegios() {
     const rol = this.userService.getRola().rola;
-    if(rol === 'IR'){
+    if (rol === 'IR') {
       this.esProfe = true;
-    }else{
+    } else {
       this.esProfe = false;
     }
+  }
+
+  // Cargar el modo preferido desde el servicio
+  cargarModoPreferido() {
+    this.modoOscuro = this.modoOscuroService.getModoOscuro();
+    if (this.modoOscuro) {
+      this.activarModoOscuro();
+    }
+  }
+
+  // Activar el modo oscuro en el body y el ion-content
+  activarModoOscuro() {
+    document.body.classList.add('dark');
+    const ionContent = document.querySelector('ion-content');
+    if (ionContent) {
+      ionContent.classList.add('dark'); // Aplicar el estilo oscuro en el ion-content
+    }
+  }
+
+  // Desactivar el modo oscuro en el body y el ion-content
+  desactivarModoOscuro() {
+    document.body.classList.remove('dark');
+    const ionContent = document.querySelector('ion-content');
+    if (ionContent) {
+      ionContent.classList.remove('dark'); // Eliminar el estilo oscuro en el ion-content
+    }
+  }
+
+  // Cambiar el modo oscuro
+  ponerModoOscuro() {
+    this.modoOscuro = !this.modoOscuro;
+    if (this.modoOscuro) {
+      this.activarModoOscuro();
+    } else {
+      this.desactivarModoOscuro();
+    }
+    this.modoOscuroService.setModoOscuro(this.modoOscuro);  // Guarda el estado en localStorage
   }
 }
