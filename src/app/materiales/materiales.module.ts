02fc1a8abd884cc925cd45c3fbb0f1e8bba98c7d
Nora <nely23@fpsanturtzilh.eus>
2025-02-06 11:52:29 +0100

    commit Nora

--- src/app/materiales/materiales.module.ts
+++ src/app/materiales/materiales.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { MaterialesPageRoutingModule } from './materiales-routing.module';
 
 import { MaterialesPage } from './materiales.page';
+import { NireKonponenteakModule } from '../nire-konponenteak/nire-konponenteak.module';
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { MaterialesPage } from './materiales.page';
     FormsModule,
     IonicModule,
     MaterialesPageRoutingModule,
+    NireKonponenteakModule
     
   ],
   declarations: [MaterialesPage]
