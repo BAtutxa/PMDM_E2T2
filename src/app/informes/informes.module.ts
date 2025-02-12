af11fe3bf7921ecabaf09fc5f91bf28224145273
AGonzalez <agonzalezb23@fpsanturtzilh.eus>
2025-02-12 09:47:50 +0100

    mas traducciones

--- src/app/informes/informes.module.ts
+++ src/app/informes/informes.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { InformesPageRoutingModule } from './informes-routing.module';
 
 import { InformesPage } from './informes.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    InformesPageRoutingModule
+    InformesPageRoutingModule,
+    TranslateModule
   ],
   declarations: [InformesPage]
 })
