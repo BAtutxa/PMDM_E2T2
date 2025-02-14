d6279a62eb4a9ec0f760ce5ac6818d4d21b65c64
agonzalezbr <agonzalezb23@fpsanturtzilh.eus>
2025-02-14 13:26:38 +0100

    TRADUCCION

--- src/app/kolore-historialak-ezabatuta/kolore-historialak-ezabatuta.module.ts
+++ src/app/kolore-historialak-ezabatuta/kolore-historialak-ezabatuta.module.ts
@@ -7,13 +7,15 @@ import { IonicModule } from '@ionic/angular';
 import { KoloreHistorialakEzabatutaPageRoutingModule } from './kolore-historialak-ezabatuta-routing.module';
 
 import { KoloreHistorialakEzabatutaPage } from './kolore-historialak-ezabatuta.page';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    KoloreHistorialakEzabatutaPageRoutingModule
+    KoloreHistorialakEzabatutaPageRoutingModule,
+    TranslateModule
   ],
   declarations: [KoloreHistorialakEzabatutaPage]
 })
